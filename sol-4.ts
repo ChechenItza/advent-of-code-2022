cecbce95b40fb416028d492c67a758377af4ce3d
Chechen Itza <artka.kazaktar@gmail.com>
2022-12-10 16:57:13 +0600

    refactoring

--- sol-4.ts
+++ sol-4.ts
@@ -1,26 +1,17 @@
 import getInput from "./input.js";
 
-async function sol1() {
-    const pairs = (await getInput(4)).split("\n")
-
-    let containedCount = 0
-    for (const pair of pairs) {
-        const [first, second] = pair.split(",")
-        if (!first || !second) continue
-
-        const [l1, r1] = first.split("-").map(val => Number(val))
-        const [l2, r2] = second.split("-").map(val => Number(val))
-        if (!l1 || !r1 || !l2 || !r2) continue
-
-        if ((l1 <= l2 && r1 >= r2) || (l2 <= l1 && r2 >= r1)) {
-            containedCount++
-        }
-    }
-
-    console.log(containedCount)
+const condition1 = (l1: number, l2: number, r1: number, r2: number) => {
+    return (l1 <= l2 && r1 >= r2) || (l2 <= l1 && r2 >= r1)
 }
 
-async function sol2() {
+const condition2 = (l1: number, l2: number, r1: number, r2: number) => {
+    return (r1 >= l2 && r1 <= r2) ||
+      (l1 >= l2 && l1 <= r2) ||
+      (r2 >= l1 && r2 <= r1) ||
+      (l2 >= l1 && l2 <= r1)
+}
+
+async function sol(condition: (l1: number, l2: number, r1: number, r2: number) => boolean) {
     const pairs = (await getInput(4)).split("\n")
 
     let overlappingCount = 0
@@ -32,12 +23,7 @@ async function sol2() {
         const [l2, r2] = second.split("-").map(val => Number(val))
         if (!l1 || !r1 || !l2 || !r2) continue
 
-        if (
-          (r1 >= l2 && r1 <= r2) ||
-          (l1 >= l2 && l1 <= r2) ||
-          (r2 >= l1 && r2 <= r1) ||
-          (l2 >= l1 && l2 <= r1)
-        ) {
+        if (condition(l1, l2, r1, r2)) {
             overlappingCount++
         }
     }
